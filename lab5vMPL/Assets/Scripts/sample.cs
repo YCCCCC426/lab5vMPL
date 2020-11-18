5eb6ceae9c17d71a972c64276b5f21271aa29a73
arthurin21 <ysun102@jh.edu>
2020-11-17 23:53:55 -0500

    upload

---
+++ lab5vMPL/Assets/Scripts/sample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sample : MonoBehaviour
+{
+    
+    private GameObject camera = null;
+    private vMPLMovementArbiter arbiter = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Camera");
+        arbiter = GameObject.Find("vMPLMovementArbiter").GetComponent<vMPLMovementArbiter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        // camera position control 
+
+        // camera rotation control
+       
+        // hand close
+
+        //hand open
+        
+
+    }
+}
